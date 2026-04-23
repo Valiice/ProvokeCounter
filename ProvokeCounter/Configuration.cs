19d80fbce8cfa28499375dda02499ec12cf0ee8c
Valiice <valentinclaes@live.be>
2026-04-23 23:47:46 +0200

    feat: update Configuration with IsOverlayVisible

--- ProvokeCounter/Configuration.cs
+++ ProvokeCounter/Configuration.cs
@@ -7,13 +7,7 @@ namespace ProvokeCounter;
 public class Configuration : IPluginConfiguration
 {
     public int Version { get; set; } = 0;
+    public bool IsOverlayVisible { get; set; } = true;
 
-    public bool IsConfigWindowMovable { get; set; } = true;
-    public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
-
-    // The below exists just to make saving less cumbersome
-    public void Save()
-    {
-        Plugin.PluginInterface.SavePluginConfig(this);
-    }
+    public void Save() => Plugin.PluginInterface.SavePluginConfig(this);
 }
