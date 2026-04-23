934fc21bd2f081f37b3af927a5ff781a8b0a66aa
Valiice <valentinclaes@live.be>
2026-04-23 23:39:21 +0200

    chore: rename SamplePlugin scaffold to ProvokeCounter

--- ProvokeCounter/Configuration.cs
+++ ProvokeCounter/Configuration.cs
@@ -1,7 +1,7 @@
 ﻿using Dalamud.Configuration;
 using System;
 
-namespace SamplePlugin;
+namespace ProvokeCounter;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
---
+++ ProvokeCounter/Plugin.cs
@@ -0,0 +1,25 @@
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace ProvokeCounter;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+
+    public Configuration Configuration { get; init; }
+
+    public Plugin()
+    {
+        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+
+        Log.Information($"ProvokeCounter loaded.");
+    }
+
+    public void Dispose()
+    {
+    }
+}
